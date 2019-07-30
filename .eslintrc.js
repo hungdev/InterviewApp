adbe7ba9e1d104ba2de48ec77d61e1da5d7a42b5
Cee <cee@Cees-MacBook-Pro.local>
2019-07-30 16:04:30 +0700

    init

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+import { rules } from '@react-native-community/eslint-config';
+module.exports = {
+  root: true,
+  extends: '@react-native-community',
+  rules: {
+    "prettier/prettier": ["error", { "singleQuote": false, "parser": "flow" }],
+  },
+};
