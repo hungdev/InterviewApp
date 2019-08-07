3ab2525a1bde269147405d4b0003e5167182b1ab
Cee <cee@Cees-MacBook-Pro.local>
2019-08-07 18:17:13 +0700

    add change admin password

--- src/containers/SettingScreen.js
+++ src/containers/SettingScreen.js
@@ -1,23 +1,26 @@
 import React from 'react';
-import {
-  View,
-  Text,
-  Image,
-  FlatList,
-  TouchableOpacity,
-  Alert
-} from 'react-native';
+import { View, Text, TouchableOpacity, TextInput, Alert } from 'react-native';
 import { connect } from 'react-redux';
 import Ionicons from 'react-native-vector-icons/Ionicons';
-import styles from './styles/HomeScreenStyles'
+import styles from './styles/SettingScreenStyles'
 import { Metrics } from '../themes';
 import NavigationService from '../appNavigation/NavigationService'
-import { removeInfoDoExam } from '../actions/questionAction'
+import { setPassword } from '../actions/authAction'
+import Modal from "react-native-modal";
 
 class SettingScreen extends React.Component {
   static navigationOptions = {
     title: 'Setting',
   };
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      isModalVisible: false,
+      adminPass: '',
+      isWrongPass: false
+    };
+  }
 
   removeHistory() {
     Alert.alert(
@@ -36,15 +39,54 @@ class SettingScreen extends React.Component {
 
   }
 
+  onPressOkModal() {
+    const { adminPass } = this.state
+    this.props.setPassword(adminPass)
+    this.setState({ isModalVisible: false })
+  }
+
   render() {
+    const { isModalVisible, adminPass, isWrongPass } = this.state;
     return (
       <View style={styles.container}>
         <TouchableOpacity style={styles.rowContent} onPress={() => this.removeHistory()}>
           <Text style={styles.txtContent}>Xóa dữ liệu thí sinh đã thi</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={styles.rowContent} onPress={() => this.onNavigate('History')}>
+        <TouchableOpacity style={styles.rowContent} onPress={() => this.setState({ isModalVisible: true })}>
           <Text style={styles.txtContent}>Đổi password admin</Text>
         </TouchableOpacity>
+        <Modal
+          isVisible={isModalVisible}
+          onBackdropPress={() => this.setState({ isModalVisible: false })}
+          swipeDirection="left"
+          backdropOpacity={0.8}
+          animationIn="zoomInDown"
+          animationOut="zoomOutUp"
+          animationInTiming={600}
+          animationOutTiming={600}
+        >
+          <View style={styles.content}>
+            <Text style={[styles.contentTitle, { fontSize: 25, marginBottom: 50, marginTop: 40 }]}>Change Admin password</Text>
+            <Text style={styles.contentTitle}>Enter new admin password </Text>
+            <TextInput
+              style={styles.input}
+              onChangeText={(text) => this.setState({ adminPass: text, isWrongPass: false })}
+              value={adminPass}
+            />
+            {isWrongPass && <Text style={styles.txtLabelTextWrong}>Password is wrong!!!</Text>}
+            <View style={styles.rowButton}>
+              <TouchableOpacity
+                style={[styles.btnBottom, { borderRightWidth: 1 }]}
+                onPress={() => this.setState({ isModalVisible: false, isWrongPass: false })}
+              >
+                <Text style={styles.txtBtnBottom}>Cancel</Text>
+              </TouchableOpacity>
+              <TouchableOpacity style={styles.btnBottom} onPress={() => this.onPressOkModal()}>
+                <Text style={styles.txtBtnBottom}>Ok</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </Modal>
       </View>
     );
   }
@@ -58,7 +100,7 @@ function mapStateToProps(state) {
 
 function mapDispatchToProps(dispatch) {
   return {
-    removeInfoDoExam: (data) => dispatch(removeInfoDoExam(data))
+    setPassword: (data) => dispatch(setPassword(data))
   }
 }
 
--- src/containers/styles/SettingScreenStyles.js
+++ src/containers/styles/SettingScreenStyles.js
@@ -1,49 +1,83 @@
 import { StyleSheet } from 'react-native'
-import { Colors, Metrics } from '../../themes';
+import { Colors, Metrics, Fonts } from '../../themes';
 
 export default StyleSheet.create({
-  containerMain: {
-    marginTop: 20,
-    backgroundColor: Colors.background,
-    flex: 1
-  },
-  cardView: {
-    marginBottom: Metrics.baseMargin
-  },
-  warpAction: {
-    borderBottomWidth: 1,
-    flexDirection: 'row',
-    justifyContent: 'space-between'
-  },
-  txtLateAction: {
-    padding: 10,
-    borderRadius: 10
-  },
-  rowTime: {
-    flexDirection: 'row',
-    justifyContent: 'space-around',
-    alignItems: 'center'
-  },
-  btnTime: {
-    borderWidth: 1,
+  container: {
     flex: 1,
-    padding: 10,
-    margin: 10,
+    // paddingVertical: 10,
+    color: Colors.white,
+    paddingTop: Metrics.baseMargin
+  },
+  warpLogo: {
+    backgroundColor: Colors.grey,
+    height: '35%',
     justifyContent: 'center',
     alignItems: 'center',
-    borderRadius: 5
   },
-  rowChannel: {
-    flexDirection: 'row',
-    // justifyContent: 'center',
-    alignItems: 'center',
-    marginLeft: 10
+  logo: {
+    alignSelf: 'center'
   },
-  warpChannelText: {
-    flexDirection: 'row',
-    // justifyContent: 'center',
+  rowContent: {
+    paddingVertical: Metrics.searchBarHeight,
+    // borderWidth: 1,
+    borderTopColor: Colors.divider,
+    paddingLeft: Metrics.baseMargin,
+    // borderBottomWidth: 1,
+    padding: Metrics.paddingButton,  //15
+    backgroundColor: Colors.card,  // '#FFFFFF'
+    // borderWidth: 1,
+    borderColor: Colors.borderGrey, // '#ccc'
+    marginBottom: Metrics.smallMargin, // 5
+    shadowColor: '#000000',
+    shadowOffset: {
+      width: 0,
+      height: 2
+    },
+    shadowRadius: 5,
+    shadowOpacity: 1.0,
+    elevation: 5,
+    marginBottom: Metrics.smallMargin
+  },
+  txtContent: {
+    fontSize: Fonts.size.input,
+  },
+  //modal
+  //modal
+  content: {
+    backgroundColor: 'white',
+    paddingTop: 22,
+    justifyContent: 'center',
     alignItems: 'center',
-    flex: 1
+    borderRadius: 4,
+    borderColor: 'rgba(0, 0, 0, 0.1)',
+  },
+  contentTitle: {
+    fontSize: 20,
+    marginBottom: 40,
+    textAlign: 'center'
+  },
+  input: {
+    height: 50,
+    width: '70%',
+    borderWidth: 1,
+    borderRadius: Metrics.smallMargin
+  },
+  rowButton: {
+    flexDirection: 'row',
+    marginTop: Metrics.plusDoubleSection,
+    borderTopWidth: 1,
+  },
+  btnBottom: {
+    // borderWidth: 1,
+    flex: 1,
+    height: 50,
+    justifyContent: 'center'
+  },
+  txtBtnBottom: {
+    textAlign: 'center'
+  },
+  txtLabelTextWrong: {
+    color: Colors.fire,
+    marginTop: Metrics.doubleBaseMargin
   }
-
 })
